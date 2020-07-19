ddddbf6dc09af9f7eeb7c4cb6c4917f225ca11d4
kristenappamondo <55129637+kristenappamondo@users.noreply.github.com>
2020-07-19 09:30:35 +0100

    add user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
